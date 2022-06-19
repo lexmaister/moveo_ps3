336260a72e784b97ff29e57f6459f27ca3301856
lexmaister <lexmaister@gmail.com>
2022-06-19 16:00:05 +0300

    added ikpy_test

---
+++ python/euler.py
@@ -0,0 +1,199 @@
+# https://programming-surgeon.com/en/euler-angle-python-en/
+
+import numpy as np
+
+def rotation_matrix(theta1, theta2, theta3, order='xyz'):
+    """
+    input
+        theta1, theta2, theta3 = rotation angles in rotation order (degrees)
+        oreder = rotation order of x,y,z e.g. XZY rotation -- 'xzy'
+    output
+        3x3 rotation matrix (numpy array)
+    """
+    c1 = np.cos(theta1 * np.pi / 180)
+    s1 = np.sin(theta1 * np.pi / 180)
+    c2 = np.cos(theta2 * np.pi / 180)
+    s2 = np.sin(theta2 * np.pi / 180)
+    c3 = np.cos(theta3 * np.pi / 180)
+    s3 = np.sin(theta3 * np.pi / 180)
+
+    if order == 'xzx':
+        matrix=np.array([[c2, -c3*s2, s2*s3],
+                         [c1*s2, c1*c2*c3-s1*s3, -c3*s1-c1*c2*s3],
+                         [s1*s2, c1*s3+c2*c3*s1, c1*c3-c2*s1*s3]])
+    elif order=='xyx':
+        matrix=np.array([[c2, s2*s3, c3*s2],
+                         [s1*s2, c1*c3-c2*s1*s3, -c1*s3-c2*c3*s1],
+                         [-c1*s2, c3*s1+c1*c2*s3, c1*c2*c3-s1*s3]])
+    elif order=='yxy':
+        matrix=np.array([[c1*c3-c2*s1*s3, s1*s2, c1*s3+c2*c3*s1],
+                         [s2*s3, c2, -c3*s2],
+                         [-c3*s1-c1*c2*s3, c1*s2, c1*c2*c3-s1*s3]])
+    elif order=='yzy':
+        matrix=np.array([[c1*c2*c3-s1*s3, -c1*s2, c3*s1+c1*c2*s3],
+                         [c3*s2, c2, s2*s3],
+                         [-c1*s3-c2*c3*s1, s1*s2, c1*c3-c2*s1*s3]])
+    elif order=='zyz':
+        matrix=np.array([[c1*c2*c3-s1*s3, -c3*s1-c1*c2*s3, c1*s2],
+                         [c1*s3+c2*c3*s1, c1*c3-c2*s1*s3, s1*s2],
+                         [-c3*s2, s2*s3, c2]])
+    elif order=='zxz':
+        matrix=np.array([[c1*c3-c2*s1*s3, -c1*s3-c2*c3*s1, s1*s2],
+                         [c3*s1+c1*c2*s3, c1*c2*c3-s1*s3, -c1*s2],
+                         [s2*s3, c3*s2, c2]])
+    elif order=='xyz':
+        matrix=np.array([[c2*c3, -c2*s3, s2],
+                         [c1*s3+c3*s1*s2, c1*c3-s1*s2*s3, -c2*s1],
+                         [s1*s3-c1*c3*s2, c3*s1+c1*s2*s3, c1*c2]])
+    elif order=='xzy':
+        matrix=np.array([[c2*c3, -s2, c2*s3],
+                         [s1*s3+c1*c3*s2, c1*c2, c1*s2*s3-c3*s1],
+                         [c3*s1*s2-c1*s3, c2*s1, c1*c3+s1*s2*s3]])
+    elif order=='yxz':
+        matrix=np.array([[c1*c3+s1*s2*s3, c3*s1*s2-c1*s3, c2*s1],
+                         [c2*s3, c2*c3, -s2],
+                         [c1*s2*s3-c3*s1, c1*c3*s2+s1*s3, c1*c2]])
+    elif order=='yzx':
+        matrix=np.array([[c1*c2, s1*s3-c1*c3*s2, c3*s1+c1*s2*s3],
+                         [s2, c2*c3, -c2*s3],
+                         [-c2*s1, c1*s3+c3*s1*s2, c1*c3-s1*s2*s3]])
+    elif order=='zyx':
+        matrix=np.array([[c1*c2, c1*s2*s3-c3*s1, s1*s3+c1*c3*s2],
+                         [c2*s1, c1*c3+s1*s2*s3, c3*s1*s2-c1*s3],
+                         [-s2, c2*s3, c2*c3]])
+    elif order=='zxy':
+        matrix=np.array([[c1*c3-s1*s2*s3, -c2*s1, c1*s3+c3*s1*s2],
+                         [c3*s1+c1*s2*s3, c1*c2, s1*s3-c1*c3*s2],
+                         [-c2*s3, s2, c2*c3]])
+
+    return matrix
+
+def rotation_angles(matrix, order):
+    """
+    input
+        matrix = 3x3 rotation matrix (numpy array)
+        oreder(str) = rotation order of x, y, z : e.g, rotation XZY -- 'xzy'
+    output
+        theta1, theta2, theta3 = rotation angles in rotation order
+    """
+    r11, r12, r13 = matrix[0]
+    r21, r22, r23 = matrix[1]
+    r31, r32, r33 = matrix[2]
+
+    if order == 'xzx':
+        theta1 = np.arctan(r31 / r21)
+        theta2 = np.arctan(r21 / (r11 * np.cos(theta1)))
+        theta3 = np.arctan(-r13 / r12)
+
+    elif order == 'xyx':
+        theta1 = np.arctan(-r21 / r31)
+        theta2 = np.arctan(-r31 / (r11 *np.cos(theta1)))
+        theta3 = np.arctan(r12 / r13)
+
+    elif order == 'yxy':
+        theta1 = np.arctan(r12 / r32)
+        theta2 = np.arctan(r32 / (r22 *np.cos(theta1)))
+        theta3 = np.arctan(-r21 / r23)
+
+    elif order == 'yzy':
+        theta1 = np.arctan(-r32 / r12)
+        theta2 = np.arctan(-r12 / (r22 *np.cos(theta1)))
+        theta3 = np.arctan(r23 / r21)
+
+    elif order == 'zyz':
+        theta1 = np.arctan(r23 / r13)
+        theta2 = np.arctan(r13 / (r33 *np.cos(theta1)))
+        theta3 = np.arctan(-r32 / r31)
+
+    elif order == 'zxz':
+        theta1 = np.arctan(-r13 / r23)
+        theta2 = np.arctan(-r23 / (r33 *np.cos(theta1)))
+        theta3 = np.arctan(r31 / r32)
+
+    elif order == 'xzy':
+        theta1 = np.arctan(r32 / r22)
+        theta2 = np.arctan(-r12 * np.cos(theta1) / r22)
+        theta3 = np.arctan(r13 / r11)
+
+    elif order == 'xyz':
+        theta1 = np.arctan(-r23 / r33)
+        theta2 = np.arctan(r13 * np.cos(theta1) / r33)
+        theta3 = np.arctan(-r12 / r11)
+
+    elif order == 'yxz':
+        theta1 = np.arctan(r13 / r33)
+        theta2 = np.arctan(-r23 * np.cos(theta1) / r33)
+        theta3 = np.arctan(r21 / r22)
+
+    elif order == 'yzx':
+        theta1 = np.arctan(-r31 / r11)
+        theta2 = np.arctan(r21 * np.cos(theta1) / r11)
+        theta3 = np.arctan(-r23 / r22)
+
+    elif order == 'zyx':
+        theta1 = np.arctan(r21 / r11)
+        theta2 = np.arctan(-r31 * np.cos(theta1) / r11)
+        theta3 = np.arctan(r32 / r33)
+
+    elif order == 'zxy':
+        theta1 = np.arctan(-r12 / r22)
+        theta2 = np.arctan(r32 * np.cos(theta1) / r22)
+        theta3 = np.arctan(-r31 / r33)
+
+    theta1 = theta1 * 180 / np.pi
+    theta2 = theta2 * 180 / np.pi
+    theta3 = theta3 * 180 / np.pi
+
+    return (theta1, theta2, theta3)
+
+def axis_rotation(theta, axis_name):
+#calculate single rotation of \(\theta\) matrix around x,y or z
+    """
+    input
+        theta = rotation angle(degrees)
+        axis_name = 'x', 'y' or 'z'
+    output
+        3x3 rotation matrix
+    """
+
+    c = np.cos(theta * np.pi / 180)
+    s = np.sin(theta * np.pi / 180)
+    if axis_name =='x':
+        rotation_matrix = np.array([[1, 0, 0],
+                                    [0, c, -s],
+                                    [0, s, c]])
+    if axis_name =='y':
+        rotation_matrix = np.array([[c, 0, s],
+                                    [0, 1, 0],
+                                    [-s, 0, c]])
+    elif axis_name =='z':
+        rotation_matrix = np.array([[c, -s, 0],
+                                    [s, c, 0],
+                                    [0, 0, 1]])
+    return rotation_matrix
+
+
+def confirm_matrix(theta1, theta2, theta3, order='xyz'):
+#Calculate Rotation matrix of Euler angles from three single rotation
+    """
+    input
+        theta1, theta2, theta3 = rotation angles in rotation order
+        order = rotation order e.g. XZY rotation -- 'xzy'
+    output
+        3x3 rotation matrix
+    """
+    axis_name1 = order[0]
+    axis_name2 = order[1]
+    axis_name3 = order[2]
+
+    rotation1 = axis_rotation(-theta1, axis_name1)
+    rotation2 = axis_rotation(-theta2, axis_name2)
+    rotation3 = axis_rotation(-theta3, axis_name3)
+
+    # rotation matrix of global world rotation around the local object
+    matrix_local = np.dot(rotation3, np.dot(rotation2, rotation1))
+
+    # inverse matrix for rotation of the local object
+    matrix_global = matrix_local.T
+
+    return matrix_global
